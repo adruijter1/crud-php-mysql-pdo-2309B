9fc0688d4ccc7ca88d943742a7754aec4ad59b3a
Arjan de Ruijter <rra@mboutrecht.nl>
2023-12-07 13:12:32 +0100

    formulier maken

--- read.php
+++ read.php
@@ -14,4 +14,54 @@ $dsn = "mysql:host=$dbHost;
         dbname=$dbName;
         charset=UTF8;";
 
+/**
+ * Maak een nieuw PDO-object om de verbinding te maken met de database op
+ * de mysql-server
+ */
 $pdo = new PDO($dsn, $dbUser, $dbPass);
+
+/**
+ * Maak een SELECT-query voor het opvragen van de data uit de databasetabel
+ * Gebruik voor het sorteren ORDER BY voor oplopen (ASC) voor aflopend (DESC)
+ */
+$sql = "SELECT Voornaam
+              ,Tussenvoegsel
+              ,Achternaam
+              ,Wachtwoord
+              ,Leeftijd
+        FROM  Persoon
+        ORDER BY Achternaam ASC";
+
+/**
+ * Maak de query geschikt voor PDO met de functie prepare
+ */
+$statement = $pdo->prepare($sql);
+
+/**
+ * Voer de query uit op de database
+ */
+$statement->execute();
+
+/**
+ * Met de fetchAll method stoppen we de geselecteerde records als object in een 
+ * array.
+ */
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <title>CRUD</title>
+</head>
+<body>
+    <h3>Persoonsgegevens</h3>
+
+    
+</body>
+</html>
